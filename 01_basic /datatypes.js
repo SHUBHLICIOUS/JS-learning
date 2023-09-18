46a572ea6790be52d7184ab5e3a64d55d32afb34
SHUBHLICIOUS <95012341+SHUBHLICIOUS@users.noreply.github.com>
2023-09-18 16:42:28 +0000

    datatype

---
+++ 01_basic /datatypes.js
@@ -0,0 +1,31 @@
+// DataTypes
+
+// number = 2 
+// string = ""
+// boolean = true/false
+// null = Standalone value
+// undefined 
+// symbol = Unique
+// array =[]
+// object ={}
+
+let name = "Shubh" //string
+let age = 24        // number
+let isLoggedIn = true // boolean
+let arrayPerson =['Shubha', 'Aman', 'Mishal'] // Array
+let obj ={'name': "Shubh", "age": 18}   // object 
+
+
+// 'typeof' is use to check the datatype of the variable
+
+console.log(typeof age) // number
+console.log(typeof  name) // string
+console.log(typeof  isLoggedIn) // boolean
+console.log(typeof  obj) // object
+
+console.log(typeof  null) // object
+console.log(typeof  undefined) // undefined
+
+
+ 
+
