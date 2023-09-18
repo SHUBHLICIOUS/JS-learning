7bd8ea35abd54a91b3982f25d9aed60a2a97fbf1
SHUBHLICIOUS <95012341+SHUBHLICIOUS@users.noreply.github.com>
2023-09-18 17:07:20 +0000

    conversion

---
+++ 01_basic /03_conversionOperation.js
@@ -0,0 +1,50 @@
+// Conversion 
+
+let marks = "74"
+
+// console.log(typeof marks);  // string
+// console.log(typeof (marks)); // string 
+
+
+let newMarks = Number(marks)
+// console.log(typeof newMarks); // number
+// console.log(typeof(newMarks)); //number
+// console.log(newMarks); // 74
+
+let someMarks ="74dshjg"
+// console.log(typeof(someMarks)); //string
+
+let newSomeMarks = Number(someMarks)
+// console.log(newSomeMarks);
+// console.log(typeof(newSomeMarks));
+
+//  "74" => 74 
+// "74dshjg" => NaN -> Not a number
+// true =>1 / false=> 0
+
+let isLoggedIn = "Shubh"
+
+let shubhIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof shubhIsLoggedIn); // boolean
+// console.log(shubhIsLoggedIn); // true
+
+let isLoggedOut = ""
+let shubhIsLoggedOut = Boolean(isLoggedOut)
+// console.log(typeof shubhIsLoggedOut); // boolean
+// console.log(shubhIsLoggedOut); // false
+
+//  "Shubh" => true
+//  "" => false
+
+let number = 32
+
+let stringOfNumber = String(number)
+
+// console.log(stringOfNumber);
+// console.log(typeof stringOfNumber);  // string
+
+
+
+
+
+
